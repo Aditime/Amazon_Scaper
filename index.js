fbd87f854109c49bab4346bc8c9cea77eb30f1d5
Aditime <46872689+Aditime@users.noreply.github.com>
2022-05-23 08:43:48 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,93 @@
+const express = require("express");
+const res = require("express/lib/response");
+const request = require("request-promise");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+const returnScraperApiUrl = (apiKey) =>
+  `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`;
+
+// Welcome route
+app.get("/", async (req, res) => {
+  res.send("Welcome to Amazon Scraper API!");
+});
+
+// Get product details
+app.get("/products/:productId", async (req, res) => {
+  const { productId } = req.params;
+  const { api_key } = req.query;
+
+  try {
+    // console.log("check: ", api_key);
+    const response = await request(
+      `${returnScraperApiUrl(
+        api_key
+      )}&url=https://www.amazon.com/dp/${productId}`
+    );
+
+    res.json(JSON.parse(response));
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+// Get product reviews
+app.get("/products/:productId/reviews", async (req, res) => {
+  const { productId } = req.params;
+  const { api_key } = req.query;
+
+  try {
+    // console.log("check: ", api_key);
+    const response = await request(
+      `${returnScraperApiUrl(
+        api_key
+      )}&url=https://www.amazon.com/product-reviews/${productId}`
+    );
+
+    res.json(JSON.parse(response));
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+// Get product offers
+app.get("/products/:productId/offers", async (req, res) => {
+  const { productId } = req.params;
+  const { api_key } = req.query;
+
+  try {
+    // console.log("check: ", api_key);
+    const response = await request(
+      `${returnScraperApiUrl(
+        api_key
+      )}&url=https://www.amazon.com/gp/offer-listing/${productId}`
+    );
+
+    res.json(JSON.parse(response));
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+// Get Search
+app.get("/search/:searchQuery", async (req, res) => {
+  const { searchQuery } = req.params;
+  const { api_key } = req.query;
+
+  try {
+    // console.log("check: ", api_key);
+    const response = await request(
+      `${returnScraperApiUrl(
+        api_key
+      )}&url=https://www.amazon.com/s?k=${searchQuery}`
+    );
+
+    res.json(JSON.parse(response));
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+app.listen(PORT, () => console.log("Sever running on port " + PORT));
